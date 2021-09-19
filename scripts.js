579b7341f489b655765cd1074c81c460b6bac479
Piyush181 <78870227+Piyush181@users.noreply.github.com>
2021-09-19 15:52:56 +0300

    Finish somewhat animating the btn

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function addClassName() {
+    let x = document.getElementById("anim_btn");
+    x.classList.add("animated_btn");
+}
+function removeClassName(){
+    let x = document.getElementById("anim_btn");
+    x.classList.remove("animated_btn");
+}
